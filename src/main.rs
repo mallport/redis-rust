7c68df74d7d7a7b44bede1d2b6f68f9649941bd7
Michael Moen Allport <mmallport@gmail.com>
2024-01-14 15:58:08 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+// Uncomment this block to pass the first stage
+use std::{io::Write, net::TcpListener};
+
+fn main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    println!("Logs from your program will appear here!");
+
+    // Uncomment this block to pass the first stage
+    //
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut _stream) => {
+                let response = "+PONG\r\n";
+                _stream.write(response.as_bytes()).unwrap();
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
